6c38c42f4d7f7795ad5670a4a0c85c10fda51df4
Max New <maxsnew@gmail.com>
2014-09-05 17:01:05 -0400

    Parse Cargo.lock

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,26 @@ extern crate toml;
 extern crate graphviz;
 
 use graphviz as dot;
+use std::io::{File};
 
 fn main() {
-    println!("Not yet implemented");
+    let contents =
+        File::open(&Path::new("Cargo.lock")).read_to_string();
+    match contents {
+        Err(e) => {
+            println!("Error: {}", e);
+            std::os::set_exit_status(1)
+        },
+        Ok(toml_str) => {
+            match toml::Parser::new(toml_str.as_slice()).parse() {
+                None => {
+                    println!("Cargo.lock is invalid toml");
+                    std::os::set_exit_status(1)
+                }
+                Some(toml) => {
+                    println!("Valid toml serialization: {}", toml::Table(toml))
+                }
+            }
+        }
+    }
 }
